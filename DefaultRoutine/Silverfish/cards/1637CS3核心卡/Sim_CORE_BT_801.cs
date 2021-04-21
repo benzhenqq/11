06b724c459c10b405cd81394fd4b29bad018e3d9
lwh <lwh@singzy.com>
2021-04-21 21:20:22 +0800

    更新常用核心卡

--- DefaultRoutine/Silverfish/cards/1637CS3核心卡/Sim_CORE_BT_801.cs
+++ DefaultRoutine/Silverfish/cards/1637CS3核心卡/Sim_CORE_BT_801.cs
@@ -8,18 +8,6 @@ namespace HREngine.Bots
 	{
         //<b>Lifesteal</b>. Deal $3 damage to a minion.<b>Outcast:</b> This costs (1).
         //<b>吸血</b>。对一个随从造成$3点伤害。<b>流放：</b>法力值消耗为（1）点。
-        public override void onCardPlay(Playfield p, bool ownplay, Minion target, int choice, bool outcast)
-        {
-            int dmg = (ownplay) ? p.getSpellDamageDamage(3) : p.getEnemySpellDamageDamage(3);
-
-            int oldHp = target.Hp;
-            p.minionGetDamageOrHeal(target, dmg);
-            if (oldHp > target.Hp) p.applySpellLifesteal(oldHp - target.Hp, ownplay);
-            if (outcast)
-            {
-                p.evaluatePenality -= 1;
-            }
-            else p.evaluatePenality += 3;
-        }
+        
     }
 }
